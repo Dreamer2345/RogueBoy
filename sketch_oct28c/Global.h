cf418e6dc206215f29a408f6734f3f1b82df3bfe
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2017-11-04 22:34:22 +0000

    Changed code with MainMenu screen

---
+++ sketch_oct28c/Global.h
@@ -0,0 +1,13 @@
+#include <Arduboy2.h>
+#include <Sprites.h>
+#include <ArduboyPlaytune.h>
+#include "Bitmaps.h"
+//Instances
+Arduboy2 ard;
+Sprites sprites;
+
+byte AniStep;
+
+
+enum class GameState { MainMenu, Settings, Game };
+GameState gameState = GameState::MainMenu;
