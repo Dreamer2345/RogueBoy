4723eea784f55cee4a8d2ddc7e34bd154087eede
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2017-11-04 02:36:04 +0000

    Update sketch_oct28b.ino

--- sketch_oct28b.ino
+++ sketch_oct28b.ino
@@ -4,63 +4,27 @@
 Arduboy2 ard;
 Sprites sprites;
 
-/*int Pressed [6] = {false,false,false,false,false,false};
-
-
-bool GETKEYS(){
-  bool Change = false;
-  bool Keys [6] = {false,false,false,false,false,false};
-  byte Key [6] = {LEFT_BUTTON,RIGHT_BUTTON,UP_BUTTON,DOWN_BUTTON,A_BUTTON,B_BUTTON};
-  for (byte i=0; i<6; i++){
-    Pressed[i] = false;
-  }
-  for (byte i=0; i<6; i++){
-    if ((Keys[i] == false)&&(ard.pressed(Key[i]))){
-      Keys[i] = true;
-      }
-    else if ((!(ard.pressed(Key[i])))&&(Keys[i] == true)){
-      Keys[i] = false;
-      Pressed[i] = true;
-      Change = true;
-      }
-  }
- return Change;
-}
-*/
-
-/*
-void DisplayBitmap(byte x,byte y,int b,bool Col){
-  uint8_t Buffer[16];
-  int Wid = 16;
-  int MemLoc = Wid*b;
-  int MemLocEnd = MemLoc+Wid;
-  for(byte i=MemLoc; i<MemLocEnd; i++){
-    Buffer[i-MemLoc] = backTiles[i];
-    }
-  if (Col){
-  ard.drawBitmap(x, y, Buffer, 16, 16, WHITE);
-  } else {
-  ard.drawBitmap(x, y, Buffer, 16, 16, BLACK);
-  }
-}
-*/
-
 void MainMenu(){
   byte Choice = 0;
+  bool Update = false;
   while(!(ard.justPressed(A_BUTTON))) {
     while(!ard.nextFrame()){} 
     ard.pollButtons();
-    if (ard.pressed(UP_BUTTON)||ard.pressed(DOWN_BUTTON)){
-      if (ard.justPressed(UP_BUTTON) && (Choice < 17)) {
+    if (ard.justPressed(UP_BUTTON) && (Choice < 18)) {
         Choice += 1;
-        }
-      if (ard.justPressed(DOWN_BUTTON) && (Choice > 0)) {
+        Update = true;
+    } 
+    if (ard.justPressed(DOWN_BUTTON) && (Choice > 0)) {
         Choice -= 1;
-        } 
+        Update = true;
+    } 
+    if (Update){
       ard.clear();
       ard.print(Choice);  
       ard.display();
+      Update = false;
     }
+    
     }
 sprites.drawSelfMasked(20,20,BackgroundEnviroment,Choice);
 ard.display();
@@ -71,12 +35,6 @@ void setup() {
   ard.begin();
   ard.clear();
 }
-
-
-
-
-
-
 
 void loop() {
  MainMenu();
