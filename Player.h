b2949c6c6de8672eb3c12c9aa7aab7bf7fddd524
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2017-11-08 12:57:22 +0000

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,45 @@
+#include <Arduboy2.h>
+#include "Maps.h"
+
+enum class GameState { MainMenu, Settings, Game };
+GameState gameState = GameState::MainMenu;
+
+class PlayerClass {
+  public:
+  int x;
+  int y;
+  byte d;
+  byte H;
+  void PlayerMovement();   
+  
+};
+
+PlayerClass playerobj;
+
+void PlayerClass::PlayerMovement(){
+  if (ard.justPressed(UP_BUTTON) && Walkable(x,y+1)){
+    y++;
+    }
+  if (ard.justPressed(DOWN_BUTTON) && Walkable(x,y-1)){
+    y--;
+    }
+  if (ard.justPressed(RIGHT_BUTTON)&& Walkable(x+1,y)){
+    x++;
+    }
+  if (ard.justPressed(LEFT_BUTTON)&& Walkable(x-1,y)){
+    x--;
+    }   
+  if (ard.pressed(A_BUTTON)&&ard.pressed(B_BUTTON)){
+    gameState = GameState::MainMenu;
+  }
+}
+
+
+
+void Init(int x,int y){
+  playerobj.x = x;
+  playerobj.y = y;
+  playerobj.d = 0;
+  playerobj.H = 100;
+}
+
