b187381078e8566a67f925a5aaa52c95262ef1aa
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2017-11-08 17:06:52 +0000

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,39 @@
+
+
+void DisplayEnviroment(){
+  int x = playerobj.x/16;
+  int y = playerobj.y/16;
+  int offx = playerobj.x % 16;
+  int offy = playerobj.y % 16;
+  for (int i=-5; i<10 ,i++;){
+    for(int j=-3; j<3 ,j++;){
+      uint8_t bl = GetBlock((i+x),(j+y));
+      int drx = (CENTERX + (i*16) + offx)-8;
+      int dry = (CENTERY + (j*16) + offy)-8;
+      sprites.drawSelfMasked(drx,dry,BackgroundEnviroment,bl);
+    }
+  }
+}
+
+void DisplayPlayer(){
+  if ((playerobj.Moving) && (ard.everyXFrames(15))){
+        playerobj.Frame = !playerobj.Frame;
+        if (playerobj.Frame){
+        sprites.drawOverwrite(CENTERX-4,CENTERY-4,SpriteEnviroment,1);
+        } else {
+        sprites.drawOverwrite(CENTERX-4,CENTERY-4,SpriteEnviroment,2);
+        }
+  } else {
+  sprites.drawOverwrite(CENTERX-4,CENTERY-4,SpriteEnviroment,0);
+  }
+  
+}
+
+
+
+void UpdateGame(){
+  playerobj.PlayerMovement();
+  DisplayEnviroment();
+  DisplayPlayer();
+}
+
---
+++ Player.h
@@ -0,0 +1,52 @@
+#include <Arduboy2.h>
+#include "Maps.h"
+
+enum class GameState { MainMenu, Settings, Game };
+GameState gameState = GameState::MainMenu;
+
+class PlayerClass {
+  public:
+  int x;
+  int y;
+  byte d;
+  byte H;
+  bool Moving;
+  bool Frame;
+  void PlayerMovement();   
+  
+};
+
+PlayerClass playerobj;
+
+void PlayerClass::PlayerMovement(){
+  Moving = false;
+  if (ard.justPressed(UP_BUTTON) && Walkable(x,y+1)){
+    y++;
+    Moving = true;
+    }
+  if (ard.justPressed(DOWN_BUTTON) && Walkable(x,y-1)){
+    y--;
+    Moving = true;
+    }
+  if (ard.justPressed(RIGHT_BUTTON)&& Walkable(x+1,y)){
+    x++;
+    Moving = true;
+    }
+  if (ard.justPressed(LEFT_BUTTON)&& Walkable(x-1,y)){
+    x--;
+    Moving = true;
+    }   
+  if (ard.pressed(A_BUTTON)&&ard.pressed(B_BUTTON)){
+    gameState = GameState::MainMenu;
+  }
+}
+
+
+
+void Init(int x,int y){
+  playerobj.x = x;
+  playerobj.y = y;
+  playerobj.d = 0;
+  playerobj.H = 100;
+}
+
