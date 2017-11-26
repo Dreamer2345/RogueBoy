67d23878d9da1698c1839f58a67ab6303f65b058
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2017-11-26 14:43:50 +0000

    Update Game.h

--- sketch_oct28c/Game.h
+++ sketch_oct28c/Game.h
@@ -94,7 +94,7 @@ void TitleText(){
           ard.print((char)pgm_read_byte(&TitleSequenceText[i]));
       }
       sprites.drawOverwrite(0,64-showarrow,Logo,0);
-      if (ard.everyXFrames(5)) { showarrow++; }
+      showarrow++;
       if (showarrow >= 64){Audio = true; showarrow = 0; gameState = GameState::MainMenu;}
   }
  }
