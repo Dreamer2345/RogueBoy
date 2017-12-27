93201b25ce67cb463ae5fe2d356795e6e2b72c03
Dreamer2345 <33360259+Dreamer2345@users.noreply.github.com>
2017-12-27 11:42:06 +0000

    Add files via upload

---
+++ RogueBoy/RogueBoy.ino
@@ -0,0 +1,45 @@
+#include <Arduboy2.h>
+#include <ArduboyTones.h>
+Arduboy2 ard;
+Sprites sprites;
+ArduboyTones sound(ard.audio.enabled);
+
+int POINTS = 0;
+bool Audio = false;
+#include "Global.h"
+#include "Game.h"
+
+
+
+
+
+void setup() {
+  ard.begin();
+  ard.clear();
+  ard.setFrameRate(30);
+  ard.initRandomSeed();
+  ard.setTextWrap(true);
+  Level = 0;
+  
+}
+
+
+
+void loop() {
+  if(!ard.nextFrame())
+    return;
+
+  ard.pollButtons();
+  ard.clear();
+
+  switch(gameState)
+  {
+    case GameState::TextTitle: TitleText(); break;
+    case GameState::MainMenu: UpdateMainMenu(); break;
+    case GameState::Game: UpdateGame(); break;
+    case GameState::LoadMap: NextLevelLoad(); break;
+    case GameState::MapEnding: MapEnding(); break;
+    case GameState::Dead: Death(); break;
+  }
+  ard.display();
+}
